fd7a8eede7c2bddbc6ac40bc25cab4184bbfe9da
Mansoor Hussain <hussainmansoor876@gmail.com>
2019-02-12 20:45:52 +0500

    Database Done

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://mansoor:mansoor11@ds113853.mlab.com:13853/nodepractice123', {useNewUrlParser: true});
+mongoose.connect('mongodb://mansoor:mansoor11@ds133275.mlab.com:33275/service-app', {useNewUrlParser: true});
 
 module.exports = mongoose
